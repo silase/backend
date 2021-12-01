16b00b06fe5f71d427bb9653bd8620b51a003155
deo12 <eunice.silas@afya.go.tz>
2021-12-01 19:18:28 +0300

    connection to mongo db

--- index.js
+++ index.js
@@ -4,17 +4,20 @@ require('dotenv').config();
 const helmet=require('helmet')
 const cors=require('cors');
 const bodyParser=require('body-parser');
+const dbconn=require('./config/config-db');
 
 
 //middleware
 app.use(helmet())
 app.use(cors())
 app.use(bodyParser.json())
+
+
 //serve setting
-app.get('/',(req,res)=>{
-
-})
-
+app.get('/api',(req,res)=>{
+    console.log(res);
+   res.send({msg:"hello"});
+});
 
 app.listen(process.env.HOST_PORT,()=>{
     console.log(`Listening .... ${process.env.HOST_PORT}`)
